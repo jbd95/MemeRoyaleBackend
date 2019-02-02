e2f003d1fc1637c703bfc276cbb20c36bdfb4740
Adarsh Pai <adarsh9pai@gmail.com>
2019-02-02 11:44:55 -0600

    testing heroku deployment

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+const PORT = process.env.PORT || 3001
 
-server.listen(procces.env.PORT)
+server.listen(PORT)
 
 app.get('/index', function (req, res) {
     res.send("Thanks for visiting")
