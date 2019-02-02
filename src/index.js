c0c9ae2957937441efb29631565dc70cdf65b69a
Ubuntu <ubuntu@ip-172-26-14-52.ec2.internal>
2019-02-02 20:01:26 +0000

    Rearrange Create Room Function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /*
-        Main Server File
+	Main Server File
 */
 
 const app = require('express')()
@@ -12,15 +12,15 @@ const DATABASE_NAME = "memeroyale"
 let All_Rooms = []
 
 http.listen(PORT, function () {
-    console.log('listening on *:', PORT)
+	console.log('listening on *:', PORT)
 })
 
 app.get('/', function (req, res) {
-    res.json({ message: "success" })
+	res.json({ message: "success" })
 })
 
 app.get('/index', function (req, res) {
-    res.send("Thanks for visiting")
+	res.send("Thanks for visiting")
 })
 
 //app.get('/room/join', function (req, res) {
@@ -29,54 +29,83 @@ app.get('/index', function (req, res) {
 
 app.get('/rooms/create', function (req, res) {
 
-    let newRoom = {
-        name: '',
-        code: randomCode(MAX_ROOM_COUNT)
-    }
+	let newRoom = {
+		name: '',
+		code: randomCode(MAX_ROOM_COUNT)
+	}
 
-    if (req.query.name) {
-        newRoom.name = req.query.name
-    }
-    else {
-        newRoom.name = newRoom.code
-    }
-    createRoom(newRoom, res)
-    //res.json({ message : "room added" })
+	if (req.query.name) {
+		newRoom.name = req.query.name
+	}
+	else {
+		newRoom.name = newRoom.code
+	}
+	createRoom(newRoom, res)
+
+
+
+	function createRoom(roomCode, res) { 
+
+		let options = {
+			database: DATABASE_NAME,
+			collectionName: "Rooms",
+			query: JSON.stringify(roomCode)
+		}
+
+		mLab.listDocuments(options, function(err, data) {
+			if (err) throw err;
+
+			if (data.length === 0)
+			{
+				delete options['query']
+				options['documents'] = roomCode
+				mLab.insertDocuments(options, function(err, data)
+				{
+					if (err) throw err;
+						res.json({message: "success"})
+				})
+			}
+			else
+			{
+				res.json({message: "fail - room already exists"})
+			}
+		})
+	}
 })
 
 app.get('/rooms', function (req, res) { 
-   
-    let options = {
-        database: DATABASE_NAME,
-	collectionName: "Rooms"
-    }
-    mLab.listDocuments(options, function(err, data) {
-        if (err) throw err;
 
-	if(data)
-	{
-		res.json({rooms : data})
+	let options = {
+		database: DATABASE_NAME,
+		collectionName: "Rooms"
 	}
-	else
-	{
-		res.json({rooms : {}})
-	}
-    })
+	mLab.listDocuments(options, function(err, data) {
+		if (err) throw err;
+
+		if(data.length !== 0)
+		{
+			res.json({rooms : data})
+		}
+		else
+		{
+			res.json({rooms : {}})
+		}
+	})
 })
 
 /* SOCKET IO COMMUNICATION */
 io.on('connection', function (socket) {
-    console.log("client connected to socket")
+	console.log("client connected to socket")
 
 
 })
 
 const randomCode = max => {
-    let proposedCode = Math.floor((Math.random() * max) + 1).toString(16).toUpperCase();
-    if (!(proposedCode in All_Rooms)) {
-        return proposedCode
-    }
-    return randomCode(max)
+	let proposedCode = Math.floor((Math.random() * max) + 1).toString(16).toUpperCase();
+	if (!(proposedCode in All_Rooms)) {
+		return proposedCode
+	}
+	return randomCode(max)
 }
 
 const func1 = token => {
@@ -90,38 +119,38 @@ const func2 = (token, val2) => {
 
 
 function createRoom(roomCode, res) { 
-    
-    let options = {
-        database: DATABASE_NAME,
-	collectionName: "Rooms",
-        query: JSON.stringify(roomCode)
-    }
 
-    mLab.listDocuments(options, function(err, data) {
-        if (err) throw err;
+	let options = {
+		database: DATABASE_NAME,
+		collectionName: "Rooms",
+		query: JSON.stringify(roomCode)
+	}
 
-	if (data.length === 0)
-	{
-	    delete options['query']
-	    options['documents'] = roomCode
-	    mLab.insertDocuments(options, function(err, data)
-	    {
+	mLab.listDocuments(options, function(err, data) {
 		if (err) throw err;
-		res.json({message: "success"})
-	    })
-	}
-	else
-	{
-	    res.json({message: "fail - room already exists"})
-	}
-    })
+
+		if (data.length === 0)
+		{
+			delete options['query']
+			options['documents'] = roomCode
+			mLab.insertDocuments(options, function(err, data)
+				{
+					if (err) throw err;
+					res.json({message: "success"})
+				})
+		}
+		else
+		{
+			res.json({message: "fail - room already exists"})
+		}
+	})
 }
 
 function joinRoom(socket, roomCode) {
-    if (roomCode in All_Rooms) {
-        socket.leave(socket.currentRoom)
-        socket.join(roomCode)
-        socket.currentRoom = roomCode
-        console.log("client changed room")
-    }
+	if (roomCode in All_Rooms) {
+		socket.leave(socket.currentRoom)
+		socket.join(roomCode)
+		socket.currentRoom = roomCode
+		console.log("client changed room")
+	}
 }
